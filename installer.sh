1f7c2c37a0169bcc0e612d68243f9dd862405b58
far3ad <farzham@gmail.com>
2018-11-01 14:55:23 -0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -5,3 +5,6 @@ sudo add-apt-repository ppa:webupd8team/java
 sudo apt update
 sudo apt install oracle-java8-installer
 sudo apt install oracle-java8-set-default
+
+# Yakuake
+sudo apt-get install yakuake
